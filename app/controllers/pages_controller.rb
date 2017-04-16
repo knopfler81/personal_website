ad920911d2fcdf925166ee91e36de913afb49969
Nelly Duclos <knopfler42@yahoo.fr>
2017-04-16 19:04:10 +0200

    added video section

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,7 @@ class PagesController < ApplicationController
 
   def projects
   end
+
+  def videos
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
   get 'projects',  to: 'pages#projects'
+  get 'videos', to: "pages#videos"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
